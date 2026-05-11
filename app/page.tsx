2fba649cdb45db67525b45d4de825b3dde0a87f2
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-05-11 18:10:20 -0300

    fix: base

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import ContactForm from "@/features/contact/components/ContactForm";
-
 export default function HomePage() {
-  return (
-    <div className="my-20 mx-70">
-      <ContactForm />
-    </div>
-  );
+  return <></>;
 }
