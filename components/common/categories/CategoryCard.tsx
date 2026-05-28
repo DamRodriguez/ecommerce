6913016e4448c6895d41bc853a43a7824f220eb4
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-05-28 15:42:34 -0300

    fix: category card hover

--- components/common/categories/CategoryCard.tsx
+++ components/common/categories/CategoryCard.tsx
@@ -16,7 +16,7 @@ type CategoryCardProps = {
 export default function CategoryCard({ data }: CategoryCardProps) {
   return (
     <Link
-      className="group relative block h-[25rem] lg:h-[35rem] overflow-hidden border border-on-surface/50 bg-surface-bright"
+      className="group hover:border-on-surface relative block h-[25rem] lg:h-[35rem] overflow-hidden border border-on-surface/50 bg-surface-bright custom-transition-all"
       href={{
         pathname: routes.shop,
         query: {
@@ -34,20 +34,18 @@ export default function CategoryCard({ data }: CategoryCardProps) {
 
       <div className="absolute inset-0 bg-black/0 custom-transition-all group-hover:bg-black/20" />
 
-      <div className="absolute inset-x-md top-md h-px origin-left scale-x-0 bg-surface custom-transition-all group-hover:scale-x-100" />
-
       <div className="absolute bottom-0 left-0 w-full border-t border-outline bg-surface-bright p-md custom-transition-all group-hover:-translate-y-sm">
         <div className="flex items-center justify-between gap-2">
-          <span className="font-accent text-xs font-medium tracking-[0.1em] uppercase text-on-surface custom-transition-all group-hover:tracking-[0.16em] line-clamp-1">
+          <span className="font-accent text-xs font-medium group-hover:font-bold tracking-[0.1em] uppercase text-on-surface custom-transition-all group-hover:tracking-[0.16em] line-clamp-1">
             {data.category}
           </span>
 
-          <span className="font-accent text-xs font-medium tracking-[0.1em] uppercase text-secondary-text line-clamp-1">
+          <span className="font-accent text-xs font-medium tracking-[0.1em] uppercase text-secondary-text line-clamp-1 group-hover:text-on-surface custom-transition-all">
             {formatTwoDigits(data.quantity)} artículos
           </span>
         </div>
 
-        <div className="mt-sm h-px w-full origin-left scale-x-0 bg-on-surface custom-transition-all group-hover:scale-x-100" />
+        <div className="mt-0 group-hover:mt-sm h-px w-full origin-left scale-x-0 bg-on-surface custom-transition-all group-hover:scale-x-100" />
       </div>
     </Link>
   );
